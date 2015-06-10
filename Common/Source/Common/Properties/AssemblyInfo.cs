e56e3b14abd49a2908b48658c4c9aba1ca691bff
Ruslan <ruslan.albu@hotmail.com>
2015-06-10 17:19:54 +0100

    description added

--- Common/Source/Common/Properties/AssemblyInfo.cs
+++ Common/Source/Common/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Common project for TeamCity Azure demo")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ruslan")]
 [assembly: AssemblyProduct("Common")]
