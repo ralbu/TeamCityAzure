118b5b701c6cbd643ac34bb5d04c20e2e2ee0e2f
Ruslan <ruslan.albu@hotmail.com>
2015-06-10 17:10:54 +0100

    company value added

--- Common/Source/Common/Properties/AssemblyInfo.cs
+++ Common/Source/Common/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ruslan")]
 [assembly: AssemblyProduct("Common")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
