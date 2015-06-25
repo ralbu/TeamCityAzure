71615262a2a9c922d6611c9f8031de7f42c896c8
Ruslan <ruslan.albu@hotmail.com>
2015-06-25 21:28:42 +0100

    testing post

--- MobileService/Source/UserManagement/Controllers/UserController.cs
+++ MobileService/Source/UserManagement/Controllers/UserController.cs
@@ -5,7 +5,9 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Common;
+using Microsoft.WindowsAzure.Mobile.Service;
 using UserManagement.Services;
+
 
 namespace UserManagement.Controllers
 {
@@ -28,14 +30,23 @@ namespace UserManagement.Controllers
             return Request.CreateResponse(_userService.GetUserById(id));
         }
 
+        public ApiServices Services { get; set; }
+
         public HttpResponseMessage Post(User user)
         {
+//            ApiServices aps = new ApiServices();
+            Services.Log.Info("Post");
             _userService.AddUser(user);
 
             var response = Request.CreateResponse(HttpStatusCode.Created, user);
 
             var host = Request.Headers.Host;
-            response.Headers.Location = new Uri(String.Format("{0}/api/user/{1}", host, user.Id));
+            Services.Log.Info("Host: " + host);
+            string uri = String.Format("{0}/api/user/{1}", host, user.Id);
+            Services.Log.Info("uri: " + uri);
+
+            response.Headers.Location = new Uri(uri);
+//            response.Headers.Location = new Uri(String.Format("{0}/api/user/{1}", host, user.Id));
 
             return response;
         }
